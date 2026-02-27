61bf76dc32521d23cac52cdeb29d8412ea7018f3
Rithesh151206 <Rithesh122006@gmail.com>
2026-02-28 00:09:10 +0530

    [Rithesh] added UC2-HardCodePalindrome

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,28 +1,48 @@
 /**
- * ============================================================================
- * MAIN CLASS - UseCase1PalindromeApp
- * ============================================================================
- * * Use Case 1: Application Entry & Welcome Message
- * * Description:
- * This class represents the entry point of the
- * Palindrome Checker Management System.
- * * At this stage, the application:
- * - Starts execution from the main() method
- * - Displays a welcome message
- * - Shows application version
- * * No palindrome logic is implemented yet.
- * * The goal is to establish a clear startup flow.
- * * @Rithesh
- * @version 1.0
+ * ================================================================
+ * MAIN CLASS - UseCase2PalindromeCheckerApp
+ * ================================================================
+ *
+ * Use Case 2: Hardcoded Palindrome Validation
+ *
+ * Description:
+ * This class demonstrates basic palindrome validation
+ * using a hardcoded string value.
+ *
+ * At this stage, the application:
+ * - Stores a predefined string
+ * - Compares characters from both ends
+ * - Determines whether the string is a palindrome
+ * - Displays the result on the console
+ *
+ * This use case introduces fundamental comparison logic
+ * before using advanced data structures.
+ *
+ * @author Rithesh
+ * @version 2.0
  */
 
-
+/**
+ * Application entry point for UC2.
+ *
+ * @param args Command-line arguments
+ */
+import java.util.Scanner;
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println("Input text:" + input);
+        boolean isPalindrome = true;
 
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Is it a palindrome? : " + isPalindrome);
     }
 }
